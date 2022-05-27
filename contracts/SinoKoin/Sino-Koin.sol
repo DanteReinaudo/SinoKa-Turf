d05c0d0de9d5320639766dc294e08dacb178ffdc
Dante Reinaudo <dantereinaudo@hotmail.com>
2022-05-27 16:17:44 -0300

    Generating our own token SinoKoin

--- contracts/SinoKoin/Sino-Koin.sol
+++ contracts/SinoKoin/Sino-Koin.sol
@@ -1,3 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./SafeMath.sol";
+
+interface ERC20Interface {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address tokenOwner) external view returns (uint balance);
+    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+    function transfer(address to, uint tokens) external returns (bool success);
+    function approve(address spender, uint tokens) external returns (bool success);
+    function transferFrom(address from, address to, uint tokens) external returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
 contract SinoKoin is ERC20Interface {
     
     using SafeMath for uint;
@@ -5,30 +22,33 @@ contract SinoKoin is ERC20Interface {
     string public constant symbol = "SNK";
     string public  constant name = "Sino-Koin";
     uint8 public constant decimals = 2;
-    uint _totalSupply;
+    uint totalSupply_;
     
     mapping(address => uint) balances;
     mapping(address => mapping(address => uint)) allowed;
+
+    //event Transfer(address indexed from, address indexed to, uint256 tokens);
+    //event Approval(address indexed owner, address indexed spender, uint256 tokens);
     
     // ------------------------------------------------------------------------
     // Constructor
     // ------------------------------------------------------------------------
-    constructor (uint256 initialSupply) public{
+    constructor (uint initialSupply) {
         totalSupply_ = initialSupply;
-        balances[msg.sender]=totalSupply_;
+        balances[msg.sender] = totalSupply_;
     }
     
     // ------------------------------------------------------------------------
     // Fixed Total supply
     // ------------------------------------------------------------------------
-    function totalSupply() public view returns (uint) {
-        return _totalSupply;
+    function totalSupply() public view override returns (uint) {
+        return totalSupply_;
     }
     
     // ------------------------------------------------------------------------
     // Get the token balance for account `tokenOwner`
     // ------------------------------------------------------------------------
-    function balanceOf(address tokenOwner) public view returns (uint balance) {
+    function balanceOf(address tokenOwner) public override view returns (uint balance) {
         return balances[tokenOwner];
     }
     
@@ -37,7 +57,8 @@ contract SinoKoin is ERC20Interface {
     // - Owner's account must have sufficient balance to transfer
     // - 0 value transfers are allowed
     // ------------------------------------------------------------------------
-    function transfer(address to, uint tokens) public returns (bool success) {
+    function transfer(address to, uint tokens) public override returns (bool success) {
+        require(tokens <= balances[msg.sender]);
         balances[msg.sender] = balances[msg.sender].sub(tokens);
         balances[to] = balances[to].add(tokens);
         emit Transfer(msg.sender, to, tokens);
@@ -52,7 +73,7 @@ contract SinoKoin is ERC20Interface {
     // recommends that there are no checks for the approval double-spend attack
     // as this should be implemented in user interfaces 
     // ------------------------------------------------------------------------
-    function approve(address spender, uint tokens) public returns (bool success) {
+    function approve(address spender, uint tokens) public  override returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
         return true;
@@ -67,7 +88,9 @@ contract SinoKoin is ERC20Interface {
     // - Spender must have sufficient allowance to transfer
     // - 0 value transfers are allowed
     // ------------------------------------------------------------------------
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+    function transferFrom(address from, address to, uint tokens) public override returns (bool success) {
+        require(tokens <= balances[from]);
+        require(tokens <= allowed[from][msg.sender]);
         balances[from] = balances[from].sub(tokens);
         allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
         balances[to] = balances[to].add(tokens);
@@ -79,66 +102,10 @@ contract SinoKoin is ERC20Interface {
     // Returns the amount of tokens approved by the owner that can be
     // transferred to the spender's account
     // ------------------------------------------------------------------------
-    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
+    function allowance(address tokenOwner, address spender) public override view returns (uint remaining) {
         return allowed[tokenOwner][spender];
     }
-    
 
-    // ------------------------------------------------------------------------
-    // Fallback function. Don't accept ETH
-    // ------------------------------------------------------------------------
-    function () public payable {
-        revert();
-    }
 
 }
 
-contract ERC20Basic is IERC20{
-string public constant name = «Observatorio Blockchain Prueba»;
-string public constant symbol =»OBP»;
-uint public constant decimals= 2;
-event Transfer(address indexed from, address indexed to, uint256 tokens);
-event Approval(address indexed owner, address indexed spender, uint256 tokens);
-using SafeMath for uint256;
-mapping(address=>uint) balances;
-mapping (address => mapping (address => uint)) allowed;
-uint256 totalSupply_;
-constructor (uint256 initialSupply) public{
-    totalSupply_ = initialSupply;
-    balances[msg.sender]=totalSupply_;
-}
-function totalsupply() public override view returns (uint256){
- return totalSupply_;
-}
-function increaseTotalSupply(uint newTokensAmount) public {
-    totalSupply_+=newTokensAmount;
-    balances[msg.sender]+= newTokensAmount;
-}
-function balanceOf(address tokenOwner) public override view returns (uint256){
-    return balances[tokenOwner];
-    }
-    function allowance(address owner, address delegate) public override view returns (uint256){
-        return allowed[owner][delegate];
-    }
-    function transfer(address recipient, uint256 numTokens) public override returns (bool){
-        require(numTokens <=balances[msg.sender]);
-        balances[msg.sender]=balances[msg.sender]. sub(numTokens);
-        balances[recipient]=balances[recipient].add(numTokens);
-        emit Transfer(msg.sender, recipient, numTokens);
-        return true;
-    }
-    function approve(address delegate, uint256 numTokens) public override returns (bool){
-        allowed[msg.sender][delegate]=numTokens;
-        emit Approval(msg.sender, delegate, numTokens);
-        return true;
-    }
-    function transferFrom(address owner, address buyer, uint256 numTokens) public override returns (bool){
-        require(numTokens <= balances[owner]);
-        require(numTokens <= allowed[owner][msg.sender]);
-        balances[owner]=balances[owner].sub(numTokens);
-        allowed[owner][msg.sender]=allowed[owner][msg.sender].sub(numTokens);
-        balances[buyer]=balances[buyer].add(numTokens);
-        emit Transfer (owner, buyer, numTokens);
-        return true;
-    }
-}
